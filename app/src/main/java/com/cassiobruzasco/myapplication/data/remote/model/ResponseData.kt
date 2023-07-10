00294a4e9cb82b3cc4bf89a419cc27ce8bb9b934
Cassio Bruzasco <cassio.nicolai@venturus.org.br>
2023-07-10 16:49:05 -0300

    Add responsedata

--- app/src/main/java/com/cassiobruzasco/myapplication/data/remote/model/ResponseData.kt
+++ app/src/main/java/com/cassiobruzasco/myapplication/data/remote/model/ResponseData.kt
@@ -1,4 +1,7 @@
 package com.cassiobruzasco.myapplication.data.remote.model
 
-class ResponseData {
+sealed class ResponseData<out T : Any> {
+    data class Success<out T : Any>(val ret: T) : ResponseData<T>()
+    data class Error(val code: Int, val errorResponse: String) : ResponseData<Nothing>()
+    data class Exception(val e: Throwable) : ResponseData<Nothing>()
 }
